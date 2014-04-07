8759fe30d8a30d1e4d9fa536f829a9b0e5a613d6
Karsten Gohm <karsten.gohm@burda.com>
2014-04-07 15:59:44 +0200

    BUNTEBIP-1534 add new admin tab to debug api calls

--- plugins/entityreference/selection/EntityReference_SelectionHandler_Tracdelight.class.php
+++ plugins/entityreference/selection/EntityReference_SelectionHandler_Tracdelight.class.php
@@ -78,13 +78,7 @@ class EntityReference_SelectionHandler_Tracdelight extends EntityReference_Selec
     // entityreference/autocomplete/single/field_edelight_products/node/edelight_gallery/75261/http%3A//td.oo34.net/cl/aaid%3DVlpQAYKIMyDSTtHO%26ein%3D21x9s0irmygjktn3%26paid%3DlYVRbN7BUaBWfHq
     // entityreference/autocomplete/single/field_edelight_products/node/edelight_gallery/75261/http%3A//editorialshopping.tracdelight.com/Bunte/muuto-elevated-vase-grau%2Cqncf1el0uy9mhaop%2Ci
     if ($query_string = arg(11)) {
-      parse_str($query_string, $query_params);
-
-      if (is_array($query_params) && isset($query_params['ein'])) {
-        return $query_params['ein'];
-      } else if (preg_match('/\,(?P<ein>[a-z0-9]{16})\,/i', $query_string, $matches)) {
-        return $matches['ein'];
-      }
+      return tracdelight_get_ein_from_uri($query_string);
     }
     return false;
   }
